692579119778c7bc1a00312b5eb8878eb2ac83de
dushyant <dushyant@fplabs.tech>
2025-03-10 23:59:46 +0530

    Model development is tested

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,82 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+# 1️⃣ DataIngestionConfig (Config Class)
+# Stores file paths (artifacts/train.csv, test.csv, data.csv).
+# Uses @dataclass to avoid writing __init__() manually.
+
+#Why Use Classes?
+#A class in Python is a blueprint for creating objects. It allows us to group data (attributes) and functions (methods) together,
+# making the code reusable and modular.
+#Reusability: Write once, use multiple times.
+#Modularity: Breaks code into manageable parts.
+
+
+# 2️⃣ DataIngestion (Processing Class)
+# Reads stud.csv, splits into train/test (80-20 split).
+# Saves train, test, and raw data into artifacts/.
+# Returns train and test file paths.
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook/data/stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+# 3️⃣ Main Execution (if __name__=="__main__")
+# Runs DataIngestion, gets train & test data paths.
+# Passes data to `Data
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
